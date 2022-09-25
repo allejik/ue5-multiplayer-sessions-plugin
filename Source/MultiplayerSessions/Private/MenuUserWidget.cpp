94d03421d0584eccc0eb2fbc028bd7c225dbfd78
Oleh P <allejik@gmail.com>
2022-09-25 22:37:08 +0200

    Add license header

--- Source/MultiplayerSessions/Private/MenuUserWidget.cpp
+++ Source/MultiplayerSessions/Private/MenuUserWidget.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2022, Oleh Pylypchuk
+// All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+
 #include "MenuUserWidget.h"
 #include "OnlineSessionSettings.h"
 #include "OnlineSubsystem.h"
--- Source/MultiplayerSessions/Private/MultiplayerSessions.cpp
+++ Source/MultiplayerSessions/Private/MultiplayerSessions.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2022, Oleh Pylypchuk
+// All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include "MultiplayerSessions.h"
 
 #define LOCTEXT_NAMESPACE "FMultiplayerSessionsModule"
